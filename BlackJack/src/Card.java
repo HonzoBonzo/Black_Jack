530b8d419fd86f53a64b2f7ccaf91550500b31ea
HonzoBonzo <konradb12@gmail.com>
2016-04-05 20:39:56 +0200

    dodalem interface ze stałymi i metode SetValue

--- BlackJack/src/Card.java
+++ BlackJack/src/Card.java
@@ -1,5 +1,5 @@
 
-public class Card {
+public class Card implements ConstValuesOfCards{
 	private String name; //2,3,4,5,6,7,8,9,10,J,Q,K,A
 	private int value; //2,3,4,5,6,7,8,9,10,11
 	private String color; // kier, pik, trefl, karo
@@ -12,14 +12,39 @@ public class Card {
 	
 	public Card(String nam, String col){
 		name = nam;
-		//value = val;
-		//SetValue(nam);
+		SetValue(nam);
 		color = col;
 	}
 	
-	public void SetValue(String namee){
-		
-		
+	public void SetValue(String nam){
+		if(nam == "TWO")
+			value = TWO;
+		else if(nam == "THREE")
+			value = THREE;
+		else if(nam == "FOUR")
+			value = FOUR;
+		else if(nam == "FIVE")
+			value = FIVE;
+		else if(nam == "SIX")
+			value = SIX;
+		else if(nam == "SEVEN")
+			value = SEVEN;
+		else if(nam == "EIGHT")
+			value = EIGHT;
+		else if(nam == "NINE")
+			value = NINE;
+		else if(nam == "TEN")
+			value = TEN;
+		else if(nam == "JUPEK")
+			value = JUPEK;
+		else if(nam == "QUEEN")
+			value = QUEEN;
+		else if(nam == "KING")
+			value = KING;
+		else if(nam == "ACE")
+			value = ACE;
+		else
+			value = -1;
 		
 	};
 	
---
+++ BlackJack/src/ConstValuesOfCards.java
@@ -0,0 +1,17 @@
+
+public interface ConstValuesOfCards {
+	int TWO = 2;
+	int THREE = 3;
+	int FOUR = 4;
+	int FIVE = 5;
+	int SIX = 6;
+	int SEVEN = 7;
+	int EIGHT = 8;
+	int NINE = 9;
+	int TEN = 10;
+	int JUPEK = 10;
+	int QUEEN = 10;
+	int KING = 10;
+	int ACE = 11;
+
+}
