eebfff05262337348c4022310fdf185d199a0a14
HonzoBonzo <konradb12@gmail.com>
2016-04-23 18:28:12 +0200

    wywalilem te metody jednak z playera wszystkie :)

--- BlackJack/src/Player.java
+++ BlackJack/src/Player.java
@@ -1,3 +1,4 @@
+import java.util.Random;
 
 public class Player {
 
@@ -5,32 +6,8 @@ public class Player {
 	private int ileKart;	//ile kart "na rêce"
 	private Card[] reka;	//kart gracza "na rêce"
 	
-	public void pokazReke(){
-		obliczSumeNaRece();
-		obliczIleKart();
-		
-		System.out.println("Posiadasz " + ileKart + "kart o lacznej wartosci: " + sumaWartosciKart);
-		
-		for(Card karta : reka){ 
-			System.out.println(karta);
-		}
-	}
 	
 	
-	public void obliczSumeNaRece()
-	{
-		sumaWartosciKart = 0;
-		for(Card karta : reka){ 
-			sumaWartosciKart += karta.value;
-		}
-	}
 	
-	public void obliczIleKart()
-	{
-		ileKart = 0;
-		for(Card karta : reka){ 
-			ileKart++;
-		}
-	}
 	
 }
