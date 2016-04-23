9a1f03d0282adb8d5743ff9728e998b58ccaa1a0
HonzoBonzo <konradb12@gmail.com>
2016-04-23 20:17:28 +0200

    dodalem Klase Messages z komunikatami

--- BlackJack/src/Gra.java
+++ BlackJack/src/Gra.java
@@ -4,13 +4,20 @@ public class Gra {
 	Player gracz;
 //	Krupier krupier;
 	public Talia tal = new Talia();
+	Boolean graszDalej;
 	
 	void start(){
 		
+		
 		tal.wypelnianie();
+		tal.tasuj();
 		gracz = new Player(tal);	
 		gracz.dobierzDwieKarty();
 		
+		while(graszDalej)
+		{
+			
+		}
 		
 		
 		//gracz.dobierzKarte();
---
+++ BlackJack/src/Messages.java
@@ -0,0 +1,20 @@
+
+public final class Messages {
+
+	public void CzyGrasz()
+	{
+		System.out.println("Graczu, czy chcesz dobrac kolejna karte?");
+	}
+	
+	public void PokazReke()
+	{
+		System.out.println("Graczu, czy chcesz zobaczyc swoja reke?");
+	}
+	
+	public void Wygrales(String s)
+	{
+		System.out.println("Przegral: " + s);
+		System.out.println("Nastepnym razem sie uda :)\nDo zobaczenia");
+	}
+	
+}
