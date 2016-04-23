a46ac4d1b194e594b10217273d7e3615cd78a9f3
HonzoBonzo <konradb12@gmail.com>
2016-04-23 18:04:16 +0200

    Dodalem metody pokazReke(), ObliczSumeNaRece() oraz obliczIleKart()

--- BlackJack/src/Player.java
+++ BlackJack/src/Player.java
@@ -1,13 +1,36 @@
 
 public class Player {
 
-	private int sumaKart;
+	private int sumaWartosciKart;
 	private int ileKart;	//ile kart "na rêce"
 	private Card[] reka;	//kart gracza "na rêce"
 	
-	void pokazReke(){
+	public void pokazReke(){
+		obliczSumeNaRece();
+		obliczIleKart();
 		
+		System.out.println("Posiadasz " + ileKart + "kart o lacznej wartosci: " + sumaWartosciKart);
+		
+		for(Card karta : reka){ 
+			System.out.println(karta);
+		}
 	}
 	
+	
+	public void obliczSumeNaRece()
+	{
+		sumaWartosciKart = 0;
+		for(Card karta : reka){ 
+			sumaWartosciKart += karta.value;
+		}
+	}
+	
+	public void obliczIleKart()
+	{
+		ileKart = 0;
+		for(Card karta : reka){ 
+			ileKart++;
+		}
+	}
 	
 }
