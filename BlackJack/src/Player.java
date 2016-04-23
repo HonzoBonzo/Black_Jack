301d771e401ab2e7f53990366b51b0ce7e9b955a
HonzoBonzo <konradb12@gmail.com>
2016-04-23 20:07:29 +0200

    Added: sumowanie wartosci kart

--- BlackJack/src/Player.java
+++ BlackJack/src/Player.java
@@ -3,7 +3,7 @@ public class Player {
 
 	private int sumaKart;
 	private int ileKart;	//ile kart "na rêce"
-	private Card[] reka = new Card[10];;	//kart gracza "na rêce"
+	private Card[] reka = new Card[10];	//kart gracza "na rêce"
 	private int zaklad; 	//ile pieniedzy ma postawione w obecnym zakladzie
 	private int pieniadze;	//ile pieniedzy ma w ogole
 	private Talia talia;
@@ -31,12 +31,15 @@ public class Player {
 		System.out.println(reka[0]);
 		System.out.println(reka[1]);
 		ileKart += 2;
+		sumaKart += reka[0].value;
+		sumaKart += reka[1].value;
 	}
 	
 	void dobierzKarte(){
 		if(talia.iterator.hasNext())
 			reka[ileKart] = (Card)talia.iterator.next();	//dobieram kolejn¹ kartê
-		ileKart++;
+		
+		sumaKart += reka[ileKart++].value;
 	}
 	
 }
